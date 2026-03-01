f39202972b9b3aeac9ef54b12e1208f49799abb4
Luan Freitas <luandkg@gmail.com>
2026-03-01 11:02:31 -0300

    🧪 test(LISTA_01_ATIVIDADE_04): Adiciona testes unitários

--- src/LISTA_01/ATIVIDADE_01_04.c
+++ src/LISTA_01/ATIVIDADE_01_04.c
@@ -31,19 +31,118 @@ int ATIVIDADE_01_04(void) {
 
 	scanf("%f",&c);
 
-	float delta = (b*b) - (4 * (a*c));
-	float raiz = sqrt(delta);
 
-
-	float x1 =0 ,x2 =0 ;
-
-	x1 =( ( (-1) * b ) + raiz)/ (2*a);
-	x2 =( ( (-1) * b ) - raiz)/ (2*a);
-
+	float x1 =equacao_segundo_grau_x1(a,b,c); 
+	float x2 =equacao_segundo_grau_x2(a,b,c);
 
 	printf("X1 = %f\n", x1);
 	printf("X2 = %f\n", x2);
 
 
 	return 0;
+}
+
+float equacao_segundo_grau_x1(float a, float b, float c) {
+
+	float delta = (b*b) - (4 * (a*c));
+	float raiz = sqrt(delta);
+
+
+	float x1 =0;
+
+	x1 =( ( (-1) * b ) + raiz)/ (2*a);
+	return x1;
+}
+
+float equacao_segundo_grau_x2(float a, float b, float c) {
+
+	float delta = (b*b) - (4 * (a*c));
+	float raiz = sqrt(delta);
+
+
+	float x2 =0 ;
+
+	x2 =( ( (-1) * b ) - raiz)/ (2*a);
+	return x2;
+}
+
+int ATIVIDADE_01_04_TESTE_EQUACAO_SEGUNDO_GRAU_X1_TESTE_01() {
+
+	
+	float x1 = equacao_segundo_grau_x1(1,-5,6);
+	                                   
+		if(x1==3.000000){
+		return 0;
+	}else{
+		return 1;
+	}
+	
+}
+
+
+int ATIVIDADE_01_04_TESTE_EQUACAO_SEGUNDO_GRAU_X2_TESTE_01() {
+
+	
+	float x2 = equacao_segundo_grau_x2(1,-5,6);
+	                                   
+		if(x2==2.000000){
+		return 0;
+	}else{
+		return 1;
+	}
+	
+}
+
+int ATIVIDADE_01_04_TESTE_EQUACAO_SEGUNDO_GRAU_X1_TESTE_02() {
+
+	
+	float x1 = equacao_segundo_grau_x1(1,-7,12);
+	                                   
+		if(x1==4.000000){
+		return 0;
+	}else{
+		return 1;
+	}
+	
+}
+
+
+int ATIVIDADE_01_04_TESTE_EQUACAO_SEGUNDO_GRAU_X2_TESTE_02() {
+
+	
+	float x2 = equacao_segundo_grau_x2(1,-7,12);
+	                                   
+		if(x2==3.000000){
+		return 0;
+	}else{
+		return 1;
+	}
+	
+}
+
+int ATIVIDADE_01_04_TESTE_EQUACAO_SEGUNDO_GRAU_X1_TESTE_03() {
+
+	
+	float x1 = equacao_segundo_grau_x1(1,-9,20);
+	                                   
+		if(x1==5.000000){
+		return 0;
+	}else{
+		return 1;
+	}
+	
+}
+
+
+int ATIVIDADE_01_04_TESTE_EQUACAO_SEGUNDO_GRAU_X2_TESTE_03() {
+
+	
+	float x2 = equacao_segundo_grau_x2(1,-9,20);
+	                                   
+		if(x2==4.000000){
+		return 0;
+	}else{
+		return 1;
+	}
+	
 }
--- src/LISTA_01/ATIVIDADE_01_04.h
+++ src/LISTA_01/ATIVIDADE_01_04.h
@@ -2,3 +2,15 @@
 #include <math.h>
 
 int ATIVIDADE_01_04(void);
+
+float equacao_segundo_grau_x1(float a, float b, float c);
+float equacao_segundo_grau_x2(float a, float b, float c);
+
+int ATIVIDADE_01_04_TESTE_EQUACAO_SEGUNDO_GRAU_X1_TESTE_01(void);
+int ATIVIDADE_01_04_TESTE_EQUACAO_SEGUNDO_GRAU_X1_TESTE_02(void);
+int ATIVIDADE_01_04_TESTE_EQUACAO_SEGUNDO_GRAU_X1_TESTE_03(void);
+
+
+int ATIVIDADE_01_04_TESTE_EQUACAO_SEGUNDO_GRAU_X2_TESTE_01(void);
+int ATIVIDADE_01_04_TESTE_EQUACAO_SEGUNDO_GRAU_X2_TESTE_02(void);
+int ATIVIDADE_01_04_TESTE_EQUACAO_SEGUNDO_GRAU_X2_TESTE_03(void);
