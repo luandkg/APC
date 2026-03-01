a410b2e8b961fac133fbc9b60add46f199c96445
Luan Freitas <luandkg@gmail.com>
2026-03-01 13:47:21 -0300

    🧪 test(LISTA_02_ATIVIDADE_04): Adiciona testes unitários
    - refs #8

--- src/LISTA_02/ATIVIDADE_02_04.c
+++ src/LISTA_02/ATIVIDADE_02_04.c
@@ -16,14 +16,14 @@ int ATIVIDADE_02_04 (void) {
 
 	printf("	### PROBLEMA DO ESPACO ###		\n");
 
-	float x1,y1,x2,y2,altura,largura ;
+	float origem_x,origem_y,altura,largura,ponto_x,ponto_y;
 
 	printf("------- PONTO DE ORIGEM ------\n");
 	printf("Informe X: ");
-	scanf("%f",&x1);
+	scanf("%f",&origem_x);
 	
 	printf("Informe Y: ");
-	scanf("%f",&y1);
+	scanf("%f",&origem_y);
 
 	printf("------- RETANGULO ------\n");
 	printf("Informe ALTURA: ");
@@ -35,38 +35,207 @@ int ATIVIDADE_02_04 (void) {
 	
 	printf("------- SEGUNDO PONTO ------\n");
 	printf("Informe X: ");
-	scanf("%f",&x2);
+	scanf("%f",&ponto_x);
 	
 	printf("Informe Y: ");
-	scanf("%f",&y2);
+	scanf("%f",&ponto_y);
 
 
-	if (x2>x1 && x2 <(x1 + largura )) {
+
+	int resposta = espaco_quadrado_ponto_dentro(origem_x,origem_y,altura,largura,ponto_x,ponto_y);	
 	
-		if (y2>y1 && y2 <(y1 + altura )) {
-			printf("DENTRO DO RETANGULO !!!\n");
-		}
-		else if (y2 == y1) {
-			printf("Y2 NA LINHA DO RETANGULO !!!\n");
-		}
-		else if (y2 == (y1 + altura )) {
-			printf("Y2 NA LINHA DO RETANGULO !!!\n");
-		} 
-		else {
-			printf("Y2 - FORA DO RETANGULO !!!\n");
-		}
-	}
-	else if (x2 == x1) {
-		printf("X2 NA LINHA DO RETANGULO !!!\n");
-	}
-	else if (x2 == (x1 + largura )) {
-		printf("X2 NA LINHA DO RETANGULO !!!\n");
-
-	} else {
-	
-		printf("X2 - FORA DO RETANGULO !!!\n");
+	if (resposta == RESPOSTA_XY_DENTRO) {
+		printf("Ponto(X,Y) - DENTRO DO RETANGULO !!!\n");
+	} else if (resposta == RESPOSTA_Y_NA_LINHA) {
+		printf("Ponto(Y) - NA LINHA DO RETANGULO !!!\n");
+	} else if (resposta == RESPOSTA_Y_FORA) {
+		printf("Ponto(Y)  - FORA DO RETANGULO !!!\n");
+	} else if (resposta == RESPOSTA_X_NA_LINHA) {
+		printf("Ponto(X) - NA LINHA DO RETANGULO !!!\n");
+	} else if (resposta == RESPOSTA_X_FORA) {
+		printf("Ponto(X) - NA LINHA DO RETANGULO !!!\n");
 	}
 	
 
-	return(0);
+	return 0;
+}
+
+
+
+int espaco_quadrado_ponto_dentro(float origem_x,float origem_y,
+                                 float altura,float largura,
+                                 float ponto_x,float ponto_y) {
+
+    if (ponto_x > origem_x && ponto_x < origem_x + largura &&
+        ponto_y > origem_y && ponto_y < origem_y + altura) {
+        return RESPOSTA_XY_DENTRO;
+    }
+
+    if ((ponto_x == origem_x) || ponto_x == (origem_x + largura)) {
+        return RESPOSTA_X_NA_LINHA;
+    }
+
+    if ((ponto_y == origem_y) || ponto_y == (origem_y + altura)) {
+        return RESPOSTA_Y_NA_LINHA;
+    }
+
+    if ((ponto_x < origem_x) || ponto_x > (origem_x + largura)) {
+        return RESPOSTA_X_FORA;
+    }
+
+    if ((ponto_y < origem_y) || ponto_y > (origem_y + altura)) {
+        return RESPOSTA_Y_FORA;
+    }
+
+    return RESPOSTA_X_FORA; 
+}
+
+int ATIVIDADE_02_04_TESTE_DENTRO_TESTE_1(void){
+
+	if(espaco_quadrado_ponto_dentro(0,0,10,10,5,5) == RESPOSTA_XY_DENTRO){
+		return TESTE_SUCESSO;
+	}else{
+		return TESTE_FALHOU;
+	}
+
+}
+
+int ATIVIDADE_02_04_TESTE_DENTRO_TESTE_2(void){
+
+	if(espaco_quadrado_ponto_dentro(0,0,10,10,2,3) == RESPOSTA_XY_DENTRO){
+		return TESTE_SUCESSO;
+	}else{
+		return TESTE_FALHOU;
+	}
+
+}
+
+int ATIVIDADE_02_04_TESTE_DENTRO_TESTE_3(void){
+
+	if(espaco_quadrado_ponto_dentro(0,0,10,10,7,8) == RESPOSTA_XY_DENTRO){
+		return TESTE_SUCESSO;
+	}else{
+		return TESTE_FALHOU;
+	}
+
+}
+
+int ATIVIDADE_02_04_TESTE_X_FORA_TESTE_1(void){
+
+	if(espaco_quadrado_ponto_dentro(0,0,10,10,15,5) == RESPOSTA_X_FORA){
+		return TESTE_SUCESSO;
+	}else{
+		return TESTE_FALHOU;
+	}
+
+}
+
+int ATIVIDADE_02_04_TESTE_X_FORA_TESTE_2(void){
+
+	if(espaco_quadrado_ponto_dentro(0,0,10,10,20,3) == RESPOSTA_X_FORA){
+		return TESTE_SUCESSO;
+	}else{
+		return TESTE_FALHOU;
+	}
+
+}
+
+int ATIVIDADE_02_04_TESTE_X_FORA_TESTE_3(void){
+
+	if(espaco_quadrado_ponto_dentro(0,0,10,10,70,8) == RESPOSTA_X_FORA){
+		return TESTE_SUCESSO;
+	}else{
+		return TESTE_FALHOU;
+	}
+
+}
+
+int ATIVIDADE_02_04_TESTE_Y_FORA_TESTE_1(void){
+
+	if(espaco_quadrado_ponto_dentro(0,0,10,10,5,50) == RESPOSTA_Y_FORA){
+		return TESTE_SUCESSO;
+	}else{
+		return TESTE_FALHOU;
+	}
+
+}
+
+int ATIVIDADE_02_04_TESTE_Y_FORA_TESTE_2(void){
+
+	if(espaco_quadrado_ponto_dentro(0,0,10,10,5,30) == RESPOSTA_Y_FORA){
+		return TESTE_SUCESSO;
+	}else{
+		return TESTE_FALHOU;
+	}
+
+}
+
+int ATIVIDADE_02_04_TESTE_Y_FORA_TESTE_3(void){
+
+	if(espaco_quadrado_ponto_dentro(0,0,10,10,5,80) == RESPOSTA_Y_FORA){
+		return TESTE_SUCESSO;
+	}else{
+		return TESTE_FALHOU;
+	}
+
+}
+
+int ATIVIDADE_02_04_TESTE_X_LINHA_TESTE_1(void){
+
+	if(espaco_quadrado_ponto_dentro(0,0,10,10,10,3) == RESPOSTA_X_NA_LINHA){
+		return TESTE_SUCESSO;
+	}else{
+		return TESTE_FALHOU;
+	}
+
+}
+
+int ATIVIDADE_02_04_TESTE_X_LINHA_TESTE_2(void){
+
+	if(espaco_quadrado_ponto_dentro(0,0,10,10,10,5) == RESPOSTA_X_NA_LINHA){
+		return TESTE_SUCESSO;
+	}else{
+		return TESTE_FALHOU;
+	}
+
+}
+
+int ATIVIDADE_02_04_TESTE_X_LINHA_TESTE_3(void){
+
+	if(espaco_quadrado_ponto_dentro(0,0,10,10,10,8) == RESPOSTA_X_NA_LINHA){
+		return TESTE_SUCESSO;
+	}else{
+		return TESTE_FALHOU;
+	}
+
+}
+
+int ATIVIDADE_02_04_TESTE_Y_LINHA_TESTE_1(void){
+
+	if(espaco_quadrado_ponto_dentro(0,0,10,10,3,10) == RESPOSTA_Y_NA_LINHA){
+		return TESTE_SUCESSO;
+	}else{
+		return TESTE_FALHOU;
+	}
+
+}
+
+int ATIVIDADE_02_04_TESTE_Y_LINHA_TESTE_2(void){
+
+	if(espaco_quadrado_ponto_dentro(0,0,10,10,5,10) == RESPOSTA_Y_NA_LINHA){
+		return TESTE_SUCESSO;
+	}else{
+		return TESTE_FALHOU;
+	}
+
+}
+
+int ATIVIDADE_02_04_TESTE_Y_LINHA_TESTE_3(void){
+
+	if(espaco_quadrado_ponto_dentro(0,0,10,10,8,10) == RESPOSTA_Y_NA_LINHA){
+		return TESTE_SUCESSO;
+	}else{
+		return TESTE_FALHOU;
+	}
+
 }
--- src/LISTA_02/ATIVIDADE_02_04.h
+++ src/LISTA_02/ATIVIDADE_02_04.h
@@ -1,4 +1,33 @@
 #include <stdio.h>
 #include <math.h>
+#include "../TESTE/TESTE.h"
 
 int ATIVIDADE_02_04 (void);
+
+static int RESPOSTA_XY_DENTRO = 0;
+static int RESPOSTA_Y_NA_LINHA = -1;
+static int RESPOSTA_X_NA_LINHA = -2;
+static int RESPOSTA_Y_FORA = -3;
+static int RESPOSTA_X_FORA = -4;
+
+int espaco_quadrado_ponto_dentro(float origem_x,float origem_y,float altura,float largura,float ponto_x,float ponto_y) ;
+
+int ATIVIDADE_02_04_TESTE_DENTRO_TESTE_1(void);
+int ATIVIDADE_02_04_TESTE_DENTRO_TESTE_2(void);
+int ATIVIDADE_02_04_TESTE_DENTRO_TESTE_3(void);
+
+int ATIVIDADE_02_04_TESTE_X_FORA_TESTE_1(void);
+int ATIVIDADE_02_04_TESTE_X_FORA_TESTE_2(void);
+int ATIVIDADE_02_04_TESTE_X_FORA_TESTE_3(void);
+
+int ATIVIDADE_02_04_TESTE_Y_FORA_TESTE_1(void);
+int ATIVIDADE_02_04_TESTE_Y_FORA_TESTE_2(void);
+int ATIVIDADE_02_04_TESTE_Y_FORA_TESTE_3(void);
+
+int ATIVIDADE_02_04_TESTE_X_LINHA_TESTE_1(void);
+int ATIVIDADE_02_04_TESTE_X_LINHA_TESTE_2(void);
+int ATIVIDADE_02_04_TESTE_X_LINHA_TESTE_3(void);
+
+int ATIVIDADE_02_04_TESTE_Y_LINHA_TESTE_1(void);
+int ATIVIDADE_02_04_TESTE_Y_LINHA_TESTE_2(void);
+int ATIVIDADE_02_04_TESTE_Y_LINHA_TESTE_3(void);
