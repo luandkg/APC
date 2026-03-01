953488b46c8b9c7aed02e5e13d212a642691856f
Luan Freitas <luandkg@gmail.com>
2026-03-01 11:23:16 -0300

    🧪 test(LISTA_01_ATIVIDADE_06): Adiciona testes unitários
    - refs #7

--- src/LISTA_01/ATIVIDADE_01_06.c
+++ src/LISTA_01/ATIVIDADE_01_06.c
@@ -4,9 +4,9 @@
  ESTUDANTE      	: Luan Alves Freitas
  MATRICULA     		: 17.0003191
  Copyright   		: FREITAS @ COPYRIGHTS
- DESCRICAO 		: LISTA DE EXERCICIOS 01 - QUESTAO 04 -- Ansi-style
- LISTA			: 01
- QUESTAO		: 06
+ DESCRICAO 			: LISTA DE EXERCICIOS 01 - QUESTAO 04 -- Ansi-style
+ LISTA				: 01
+ QUESTAO			: 06
  ============================================================================
  */
 
@@ -52,9 +52,7 @@ int ATIVIDADE_01_06(void) {
 
 	scanf("%d",&peso3);
 
-
-	notafinal = ( (nota1*peso1) + (nota2*peso2) + (nota3*peso3)  ) / ( peso1 + peso2 + peso3 );
-
+	notafinal = calcular_media_ponderada(nota1,peso1,nota2,peso2,nota3,peso3);
 
 	printf("------------------------------\n");
 
@@ -62,4 +60,36 @@ int ATIVIDADE_01_06(void) {
 
 
 	return 0;
+}
+
+float calcular_media_ponderada(float nota1,int peso1,float nota2,int peso2,float nota3,int peso3){
+	return ( (nota1*peso1) + (nota2*peso2) + (nota3*peso3)  ) / ( peso1 + peso2 + peso3 );
+}
+
+int ATIVIDADE_01_06_TESTE_CALCULA_NOTA_MEDIA_PONDERADA_TESTE_01(void) {
+
+	float nota = calcular_media_ponderada(2,30,5,30,10,40);                       
+	if((int)nota==6){
+		return 0;
+	}else{
+		return 1;
+	}
+}
+
+int ATIVIDADE_01_06_TESTE_CALCULA_NOTA_MEDIA_PONDERADA_TESTE_02(void) {
+	float nota = calcular_media_ponderada(5,30,10,30,8,40);                       
+	if((int)nota==7){
+		return 0;
+	}else{
+		return 1;
+	}
+}
+
+int ATIVIDADE_01_06_TESTE_CALCULA_NOTA_MEDIA_PONDERADA_TESTE_03(void) {
+	float nota = calcular_media_ponderada(1,30,2,30,4,40);                       
+	if((int)nota==2){
+		return 0;
+	}else{
+		return 1;
+	}
 }
--- src/LISTA_01/ATIVIDADE_01_06.h
+++ src/LISTA_01/ATIVIDADE_01_06.h
@@ -2,3 +2,8 @@
 #include <math.h>
 
 int ATIVIDADE_01_06(void);
+float calcular_media_ponderada(float nota1,int peso1,float nota2,int peso2,float nota3,int peso3);
+
+int ATIVIDADE_01_06_TESTE_CALCULA_NOTA_MEDIA_PONDERADA_TESTE_01(void);
+int ATIVIDADE_01_06_TESTE_CALCULA_NOTA_MEDIA_PONDERADA_TESTE_02(void);
+int ATIVIDADE_01_06_TESTE_CALCULA_NOTA_MEDIA_PONDERADA_TESTE_03(void);
